55a5313e68aefa2399ea9481922fbfc7f28b5afa
Eugene Cheung <cheueuge@amazon.com>
2024-06-25 14:22:26 -0400

    chore: enforce trailing comma style

--- lib/common/monitoring/alarms/QueueAlarmFactory.ts
+++ lib/common/monitoring/alarms/QueueAlarmFactory.ts
@@ -43,7 +43,7 @@ export class QueueAlarmFactory {
   addMinQueueMessageCountAlarm(
     metric: MetricWithAlarmSupport,
     props: MinMessageCountThreshold,
-    disambiguator?: string
+    disambiguator?: string,
   ) {
     return this.alarmFactory.addAlarm(metric, {
       treatMissingData:
@@ -64,7 +64,7 @@ export class QueueAlarmFactory {
   addMaxQueueMessageCountAlarm(
     metric: MetricWithAlarmSupport,
     props: MaxMessageCountThreshold,
-    disambiguator?: string
+    disambiguator?: string,
   ) {
     return this.alarmFactory.addAlarm(metric, {
       treatMissingData:
@@ -85,7 +85,7 @@ export class QueueAlarmFactory {
   addMaxQueueMessageAgeAlarm(
     metric: MetricWithAlarmSupport,
     props: MaxMessageAgeThreshold,
-    disambiguator?: string
+    disambiguator?: string,
   ) {
     return this.alarmFactory.addAlarm(metric, {
       treatMissingData:
@@ -104,7 +104,7 @@ export class QueueAlarmFactory {
   addMaxQueueTimeToDrainMessagesAlarm(
     metric: MetricWithAlarmSupport,
     props: MaxTimeToDrainThreshold,
-    disambiguator?: string
+    disambiguator?: string,
   ) {
     return this.alarmFactory.addAlarm(metric, {
       treatMissingData:
@@ -123,7 +123,7 @@ export class QueueAlarmFactory {
   addMinQueueIncomingMessagesCountAlarm(
     metric: MetricWithAlarmSupport,
     props: MinIncomingMessagesCountThreshold,
-    disambiguator?: string
+    disambiguator?: string,
   ) {
     return this.alarmFactory.addAlarm(metric, {
       treatMissingData:
@@ -142,7 +142,7 @@ export class QueueAlarmFactory {
   addMaxQueueIncomingMessagesCountAlarm(
     metric: MetricWithAlarmSupport,
     props: MaxIncomingMessagesCountThreshold,
-    disambiguator?: string
+    disambiguator?: string,
   ) {
     return this.alarmFactory.addAlarm(metric, {
       treatMissingData:
